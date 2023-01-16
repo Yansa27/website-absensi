7c48f6d96593d1e6d11702496d7432138060d7ea
Yansa27 <juliansa837@gmail.com>
2023-01-17 00:18:00 +0700

    push

--- login.php
+++ login.php
@@ -1,20 +1,21 @@
 <?php
+
 include("conection.php");
-include("users_class.php");
+include("users.php");
+
 $user = new Users();
 
-
-
 if (isset($_POST['login'])) {
+    $user->set_login_data($_POST['user_id'], $_POST['password']);
 
-    $user->set_login_data($_POST['user_id'],  $_POST['password']);
+    $user_id = $user->get_user_id();
+    $password = $user->get_password();
 
-    $sql = "SELECT * FROM users WHERE user_id=$user_id AND password='$password'";
+    $sql = "SELECT * FROM users WHERE user_id='$user_id' AND password='$password'";
+
     $result = $db->query($sql);
 
     if ($result->num_rows > 0) {
-
-
         while ($data = $result->fetch_assoc()) {
             session_start();
             $_SESSION['user_id'] = $data['user_id'];
@@ -22,9 +23,9 @@ if (isset($_POST['login'])) {
             $_SESSION['role'] = $data['role'];
             $_SESSION['status'] = "login";
 
-            header("location:dashboard/index.php?message=selamat datang di sistem absensi sederhana");
+            header("location:dashboard/index.php?message=selamat datang di sistem absensi sederhana 🎉");
         }
     } else {
-        echo "login gagal";
+        header("location:index.php?message=silahkan masukan data yang benar 😉");
     }
 }
